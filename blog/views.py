414979b2ad763f0275946d8abeb2ef5864dd3e9f
Muzzocourt <muzzolonjulie@yahoo.fr>
2023-02-01 21:01:32 +0100

    modif

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+# pour importer le modele situé dans models.py
+from .models import Post
+# pour importer les fichiers par date 
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
